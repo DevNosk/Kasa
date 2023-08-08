7fde91393ac5f5ebf3384f3f7691416a8e7cd0a8
Tom <Tissot.tom31@gmail.com>
2023-08-08 11:19:51 +0200

    Create components Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <p>Bonjour Je suis le bas de page</p>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <p>Bonjour Je suis l'entête</p>
+  )
+}
+
+export default Header;
