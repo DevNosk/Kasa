b4a209f796cc0c0e52a4f309823a11005de6438c
Tom <Tissot.tom31@gmail.com>
2023-08-17 10:23:14 +0200

    Create components Header and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import React from "react";
-
-
-function Footer() {
-  return (
-    <p>Bonjour Je suis le bas de page</p>
-  )
+import React from 'react';
+import white_logo from '../assets/white_logo.png';
+import '../style/Footer.css';
+export default function Footer() {
+	return (
+		<footer>
+			<img src={white_logo} alt="Logo" />
+			<p>© 2020 Kasa. All rights reserved</p>
+		</footer>
+	);
 }
-
-export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
-import React from "react";
-
-
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import '../style/Header.css';
 function Header() {
-  return (
-    <p>Bonjour Je suis l'entête</p>
-  )
+	return (
+		<div className="header">
+			<img src={logo} alt="Logo" />
+			<div className="nav">
+				<NavLink
+					to="/a-propos"
+					className={({ isActive, isPending }) =>
+						isPending ? 'pending' : isActive ? 'active' : ''
+					}
+				>
+					A Propos
+				</NavLink>
+
+				<NavLink
+					to="/"
+					className={({ isActive, isPending }) =>
+						isPending ? 'pending' : isActive ? 'active' : ''
+					}
+				>
+					Accueil
+				</NavLink>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
