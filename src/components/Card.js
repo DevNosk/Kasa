f0e633834a46b7402f3ecc66385c8047fa35151a
Tom <Tissot.tom31@gmail.com>
2023-08-17 10:22:29 +0200

    Create component Card

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Card.css';
+
+export default function Card({ location }) {
+	return (
+		<Link to={'/fiche-logement/' + location.id} key={'' + location.id}>
+			<article>
+				<img src={location.cover} alt={location.title} />
+				<h2>{location.title}</h2>
+			</article>
+		</Link>
+	);
+}
