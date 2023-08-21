9edaeea37f48c77260ca0cfbfb27089795a8c571
Tom <Tissot.tom31@gmail.com>
2023-08-21 10:59:09 +0200

    Change name Housings for housings

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Banner from './Banner';
 import Card from './Card';
-import Housings from '../assets/data/housings.json';
+import housings from '../assets/data/housings.json';
 
 export default function Home() {
 	const housing = useLocation();
@@ -11,7 +11,7 @@ export default function Home() {
 		<div>
 			<Banner location={housing} />
 			<section className="cardsContainer">
-				{Housings.map((housing, index) => (
+				{housings.map((housing, index) => (
 					<Card location={housing} key={index} />
 				))}
 			</section>
