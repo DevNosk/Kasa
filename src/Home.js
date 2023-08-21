f6d5814769ba889f750728587823a5b0fafaaa15
Tom <Tissot.tom31@gmail.com>
2023-08-21 10:55:43 +0200

    Change folder SRC for folder components

--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Home() {
-  return (
-    <p>Bonjour</p>
-  )
-}
-
-export default Home;
--- src/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function NotFound() {
-  return (
-    <p>Au revoir</p>
-  )
-}
-
-export default NotFound;
