68e21c030e316d921d9297283e947000eade12d6
Tom <Tissot.tom31@gmail.com>
2023-08-21 10:59:38 +0200

    Create app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RouterKasa from "./RouterKasa"
+
+function App() {
+  return (
+    <React.Fragment>
+     <RouterKasa  /> 
+     </React.Fragment>
+  );
+}
+
+export default App;
