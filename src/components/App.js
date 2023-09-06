7bc960417f12f3d523f27e96c299560a7f571dd9
Tom <Tissot.tom31@gmail.com>
2023-09-06 09:10:03 +0200

    import typo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import RouterKasa from "./RouterKasa"
-
+import React from 'react';
+import RouterKasa from './RouterKasa';
+import '../style/typography.css';
 function App() {
-  return (
-    <React.Fragment>
-     <RouterKasa  /> 
-     </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<RouterKasa />
+		</React.Fragment>
+	);
 }
 
 export default App;
